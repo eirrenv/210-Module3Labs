9119e6963d4828722d18634648f222e5e9570398
Eirren <edv2019@gmail.com>
2026-02-10 14:58:28 -0800

    enterArrayData() setup

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+// function prototypes
+double enterArrayData(double *dynamicArray);
+// void outputArrayData(double *dynArray);
+// double sumArray(double *dArray);
+
+// array size initialization
+const int SIZE = 5;
+
 int main() {
 
     return 0;
+}
+
+double enterArrayData(double *dynamicArray) {
+
 }
