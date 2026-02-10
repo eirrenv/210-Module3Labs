8387de50151861fee2785f5c062a1f5d74116040
Eirren <edv2019@gmail.com>
2026-02-10 14:41:18 -0800

    Pointerified and tested code

--- Lab_5A.cpp
+++ Lab_5A.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int swap(int &, int &);
+// changed function to void and inputs into pointers
+void swap(int*, int*);
 
 int main() {
     int x = 5, y = 10;
-    cout << "x = " << x << "  | y = " << y << endl;
+
+    // created new pointers for x and y
+    int *xptr = nullptr;
+    int *yptr = nullptr;
+
+    // set pointer addresses to x and y locations
+    xptr = &x;
+    yptr = &y;
+
+    // changed variable calls to pointer calls
+    cout << "x = " << *xptr << "  | y = " << *yptr << endl;
 
     cout << "Swapping...\n";
-    int sum = swap(x, y);
+
+    // swapping values using pointers
+    swap(xptr, yptr);
+
+    // declaring sum variable and adding newly swapped values outside of previous function
+    int sum = 0;
+    sum = x + y;
+
     cout << "x = " << x << " | y = " << y << endl;
     cout << "sum = " << sum << endl;
     return 0;
 }
 
-int swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
-    return a + b;
+
+// changed variables to pointers
+void swap(int *xpointer, int *ypointer) {
+    int temp = *xpointer;
+    *xpointer = *ypointer;
+    *ypointer = temp;
 }
-// code to commit to correct repo
